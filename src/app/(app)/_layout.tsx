280cb4f91a90c9f7b2e4d3805ef0ed7055ff0a67
Duc Tran <ducvtrann@gmail.com>
2024-10-28 22:28:27 -0400

    04 - Add icons to bottom tab navigation

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,16 +1,53 @@
 import { Tabs } from 'expo-router';
+import { House, PlusCircle, Search, Trophy, User } from 'lucide-react-native';
+import { theme } from '@/theme/theme';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: 'black',
+        tabBarActiveTintColor: theme.colorGreen,
       }}>
-      <Tabs.Screen name="(play)" options={{ headerShown: false }} />
-      <Tabs.Screen name="(explore)" options={{ headerShown: false }} />
-      <Tabs.Screen name="(create)" options={{ headerShown: false }} />
-      <Tabs.Screen name="(ranking)" options={{ headerShown: false }} />
-      <Tabs.Screen name="(profile)" options={{ headerShown: false }} />
+      <Tabs.Screen
+        name="(play)"
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => <House size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="(explore)"
+        options={{
+          title: 'Explore',
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => <Search size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="(create)"
+        options={{
+          title: 'Create',
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => <PlusCircle size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="(ranking)"
+        options={{
+          title: 'Ranking',
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => <Trophy size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="(profile)"
+        options={{
+          title: 'Profile',
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => <User size={size} color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
